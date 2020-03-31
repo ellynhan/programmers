7b938c65152463e29f962d37c4f72fcd5c8e136d
ellynhan <38302837+ellynhan@users.noreply.github.com>
2020-04-01 00:44:42 +0900

    other's

---
+++ printer_maxelement.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    int max = *max_element(priorities.begin(), priorities.end());
+    while (true)
+    {
+        for (int i = 0; i < priorities.size(); ++i)
+        {
+            if (priorities[i] == max)
+            {
+                ++answer;
+
+                if (i == location)
+                    return answer;
+
+                priorities[i] = 0;
+                max = *max_element(priorities.begin(), priorities.end());
+            }
+        }
+    }
+}
