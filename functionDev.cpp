130b7167a3585672a166a762deaaa19e4b706775
ellynhan <38302837+ellynhan@users.noreply.github.com>
2020-03-31 23:58:24 +0900

    Update functionDev.cpp

--- functionDev.cpp
+++ functionDev.cpp
@@ -8,15 +8,13 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
         for(int i=0; i<progresses.size(); i++){
             progresses[i]+=speeds[i];
         }
-        if(*progresses.begin()>=100){
-            int result=0;
-            while(*progresses.begin()>=100&&!progresses.empty()){
-                result++;
-                progresses.erase(progresses.begin());
-                speeds.erase(speeds.begin());
-            }
-            answer.push_back(result);
+        int result=0;
+        while(*progresses.begin()>=100&&!progresses.empty()){
+            result++;
+            progresses.erase(progresses.begin());
+            speeds.erase(speeds.begin());
         }
+        if(result!=0)answer.push_back(result);
     }
     return answer;
 }
