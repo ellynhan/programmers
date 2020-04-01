7819bf269f0b0626dde786bc1952c9ae49d4ace4
ellynhan <38302837+ellynhan@users.noreply.github.com>
2020-04-01 23:14:43 +0900

    other's

---
+++ laser_stack.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string a) {
+    int answer = 0;
+    stack<char> s;
+
+    for(int i=0 ; i<a.length();i++){
+        if(a[i] =='(')
+            s.push(a[i]);
+        else{
+            s.pop();
+            if(a[i-1] =='(')
+                answer += s.size();
+            else
+                answer ++;
+        }
+    }
+    return answer;
+}
