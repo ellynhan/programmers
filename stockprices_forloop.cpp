c17afd98fc61b47a8d57a91ca30100b379f45977
ellynhan <38302837+ellynhan@users.noreply.github.com>
2020-04-08 20:57:48 +0900

    이중 반복문이 어떻게 효율성테스트를 통과했지?

---
+++ stockprices_forloop.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    int size=prices.size();
+
+    for(int i=0; i<size; i++){
+        int time=0;
+        for(int j=i+1; j<size; j++){
+            time++;
+            if(prices[j]<prices[i]||j==size-1){ //마지막에서 두번째 값은 만약 마지막 값이 더 크다면 push할 수가 없기때문에 조건에 추가
+                answer.push_back(time);
+                break;
+            }
+        }
+    }
+
+    answer.push_back(0); //마지막은 시간이 없음
+
+    return answer;
+}
