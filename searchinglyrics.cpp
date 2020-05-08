89093a4ee84b449510a0559822c1b1bd9c9359a9
ellynhan <38302837+ellynhan@users.noreply.github.com>
2020-05-08 23:33:48 +0900

    Trie Tree Algorithm | 2020 kakao blind

--- searchinglyrics.cpp
+++ searchinglyrics.cpp
@@ -1,15 +1,14 @@
-#include <cstring>
+#include <string>
 #include <vector>
-#include <queue>
+#include <cstring>
 #include <algorithm>
-
 using namespace std;
 class Trie{
 public:
     Trie *children[26];
-    int depth=0;
+    int cnt=0;
     bool isTerminal=false;
-    Trie(int a) : isTerminal(false),depth(a) {
+    Trie() : isTerminal(false) {
            memset(children, NULL, sizeof(children));
     }
     ~Trie() {
@@ -26,13 +25,13 @@ public:
         {
             int index = key[i] - 'a';
             if (!p->children[index])
-                p->children[index] = new Trie((p->depth)+1);
-    
+                p->children[index] = new Trie();
+            p->cnt++;
             p = p->children[index];
         }
         p->isTerminal = true;
     }
-    Trie* search(string key)
+    int search(string key)
     {
         Trie *p = this;
 
@@ -40,54 +39,45 @@ public:
         {
             int index = key[i] - 'a';
             if (!p->children[index])
-                return NULL;
+                return 0;
             
             p = p->children[index];
         }
 
-        return p;
-    }
-    int counting(string part, int len){
-        Trie *root = search(part);
-        if(root==NULL)return 0;
-        int cnt =0;
-        queue<Trie*> q;
-        q.push(root);
-        while(!q.empty()){
-            Trie *p = q.front();
-            q.pop();
-            for(int i=0; i<26; i++){
-                if(p->children[i]!=NULL){
-                    q.push(p->children[i]);
-                    if(p->children[i]->depth==len&&p->children[i]->isTerminal)cnt++;
-                }
-            }
-        }
-        return cnt;
+        return p->cnt;
     }
 };
+Trie *proot[10000]; // preffix
+Trie *sroot[10000]; // suffix
+
 vector<int> solution(vector<string> words, vector<string> queries) {
     vector<int>answer(queries.size());
-    Trie proot(0);
-    Trie sroot(0);
     for(int i=0; i<words.size(); i++){
-        proot.insert(words[i]);
+        int len = int(words[i].size());
+        if(proot[len]==NULL)proot[len]=new Trie();
+        if(sroot[len]==NULL)sroot[len]=new Trie();
+        proot[len]->insert(words[i]);
         reverse(words[i].begin(),words[i].end());
-        sroot.insert(words[i]);
+        sroot[len]->insert(words[i]);
     }
+    
     for(int i=0; i<queries.size(); i++){
-        int len = queries[i].length();
+        int len = int(queries[i].length());
+        if(proot[len]==NULL||sroot[len]==NULL){
+            answer[i]=0;
+            continue;
+        }
         string tmp;
         int cnt=0;
         if(queries[i][0]=='?'){
             while(queries[i][cnt]=='?'){cnt++;}
             tmp = queries[i].substr(cnt,len-cnt);
             reverse(tmp.begin(),tmp.end());
-            answer[i]=sroot.counting(tmp,len);
+            answer[i]=sroot[len]->search(tmp);
         }else{
             while(queries[i][len-cnt-1]=='?'){cnt++;}
             tmp = queries[i].substr(0,len-cnt);
-            answer[i]=proot.counting(tmp,len);
+            answer[i]=proot[len]->search(tmp);
         }
     }
     return answer;
